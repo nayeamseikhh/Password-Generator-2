36d80dc8c00eaceba367c515d5e445ba4a9bb31b
Nayeam <nayeam@Nayeams-Mac-mini.local>
2025-12-24 10:02:53 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let viewPassword = document.querySelector(".viewPassword");
+let generatePassword = document.querySelector(".generatePassword");
+let copyPassword = document.querySelector(".copyPassword");
+
+generatePassword.addEventListener("click", function () {
+  let passwordLength = 18;
+  viewPassword.value = PasswordGenerator(passwordLength);
+});
+
+copyPassword.addEventListener("click", clickPassword);
+
+function PasswordGenerator(passwordLength) {
+  let Password = "";
+  let Character =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=!@#$%^&*()_+[]{};:<>?";
+
+  for (let i = 0; i < passwordLength; i++) {
+    Password += Character[Math.floor(Math.random() * Character.length)];
+  }
+  return Password;
+}
+
+function clickPassword() {
+  viewPassword.select();
+  copyPassword.innerHTML = "📋";
+}
